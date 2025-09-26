14059031935e9bc4939182c47d9fe6f85a7129be
woxQAQ <woxqaq@gmail.com>
2025-09-26 22:09:47 +0800

    chore: update nixd

---
+++ modules/nixvim/lang/_nixd_expr_.nix
@@ -0,0 +1,47 @@
+{ system, self, ... }:
+let
+  inherit (builtins)
+    isString
+    filter
+    split
+    concatStringsSep
+    readDir
+    length
+    genList
+    elemAt
+    getEnv
+    ;
+  dropLast =
+    list:
+    let
+      len = length list;
+      dropped = genList (elemAt list) (len - 1);
+    in
+    if list == [ ] || len == 1 then [ ] else dropped;
+  ispart = part: isString part && part != "" && part != ".";
+  parts = dir: filter ispart (split "/+" dir);
+  findFlake' =
+    parts:
+    let
+      dir = "/" + concatStringsSep "/" parts;
+      files = readDir dir;
+      isFlake = files."flake.nix" or null == "regular";
+      parent = dropLast parts;
+    in
+    if parts == [ ] then
+      null
+    else if isFlake then
+      dir
+    else
+      findFlake' parent;
+  findFlake = dir: findFlake' (parts dir);
+  pwd = getEnv "PWD";
+  path = findFlake pwd;
+in
+{
+
+  inherit system self path;
+
+  local = if path == null then null else builtins.getFlake path;
+  global = builtins.getFlake self;
+}
--- modules/nixvim/lang/nixd.nix
+++ modules/nixvim/lang/nixd.nix
@@ -1,10 +1,37 @@
 {
+  lib,
+  pkgs,
+  self,
+  ...
+}:
+let
+  inherit (builtins) toFile toJSON;
+  inherit (pkgs.stdenv.hostPlatform) system;
+  wrapper = toFile "expr.nix" ''
+    import ${./_nixd_expr_.nix} {
+      self = ${toJSON self};
+      system = ${toJSON system};
+    }'';
+  withFlake = expr: "with import ${wrapper};" + expr;
+in
+{
   lsp.servers.nixd = {
     enable = true;
-    settings.settings = {
-      nixpkgs.expr = "import <nixpkgs> { }";
-      formatting = {
-        command = [ "nixfmt" ];
+    settings.settings.nixd = {
+      formatting.command = [ "${lib.getExe pkgs.nixfmt}" ];
+      nixpkgs.expr =
+        withFlake
+          # nix
+          ''
+            import (
+              if local ? lib.version then local
+            else local.inputs.nixpkgs
+              or global.inputs.nixpkgs
+            ) { }
+          '';
+      options = {
+        nixvim.expr = withFlake "global.nixvimConfigurations.\${system}.default.options";
+        flake-parts.expr = withFlake "local.debug.options or global.debug.options";
       };
     };
   };
