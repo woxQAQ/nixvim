9d8dfacbb0d2df31b5aa605eef593938e7fcdebd
woxQAQ <woxqaq@gmail.com>
2026-01-09 17:14:11 +0800

    chore: treesitter update

--- modules/nixvim/plugins/treesitter/default.nix
+++ modules/nixvim/plugins/treesitter/default.nix
@@ -8,34 +8,8 @@
       enable = true;
 
       folding.enable = true;
-      # nixvimInjections = true;
-
-      settings = {
-        highlight = {
-          additional_vim_regex_highlighting = true;
-          enable = true;
-          disable = # Lua
-            ''
-              function(lang, bufnr)
-                return vim.api.nvim_buf_line_count(bufnr) > 10000
-              end
-            '';
-        };
-
-        incremental_selection = {
-          enable = true;
-          keymaps = {
-            init_selection = "gnn";
-            node_incremental = "grn";
-            scope_incremental = "grc";
-            node_decremental = "grm";
-          };
-        };
-
-        indent = {
-          enable = true;
-        };
-      };
+      highlight.enable = true;
+      indent.enable = true;
     };
 
     treesitter-context = {
