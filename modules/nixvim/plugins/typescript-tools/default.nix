17de146a251aa34babac119f28f4ad080931d6c4
woxQAQ <woxqaq@gmail.com>
2026-01-09 16:44:12 +0800

    chore: update ts-tools settings

--- modules/nixvim/plugins/typescript-tools/default.nix
+++ modules/nixvim/plugins/typescript-tools/default.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   plugins.typescript-tools = {
     enable = true;
@@ -7,5 +8,12 @@
       "typescriptreact"
       "javascriptreact"
     ];
+    settings.settings = {
+      tsserver_path = "${pkgs.typescript}/lib/node_modules/typescript/lib/tsserver.js";
+      code_lens = "off";
+      expose_as_code_action = "all";
+      include_completions_with_insert_text = true;
+      separate_diagnostic_server = true;
+    };
   };
 }
