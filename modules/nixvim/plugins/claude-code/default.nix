eb72f19e5b64fbfe71155df39b29e6726b2b8077
woxQAQ <woxqaq@gmail.com>
2026-01-21 02:59:11 +0800

    fix

--- modules/nixvim/plugins/claude-code/default.nix
+++ modules/nixvim/plugins/claude-code/default.nix
@@ -1,25 +1,19 @@
-{ lib, config, ... }:
+# { ... }:
 {
-  config = lib.mkMerge [
-    {
-      plugins.claude-code = {
-        enable = false;
-        lazyLoad.settings.cmd = [
-          "ClaudeCode"
-          "ClaudeCodeContinue"
-          "ClaudeCodeResume"
-          "ClaudeCodeVerbose"
-        ];
+  plugins.claude-code = {
+    enable = false;
+    lazyLoad.settings.cmd = [
+      "ClaudeCode"
+      "ClaudeCodeContinue"
+      "ClaudeCodeResume"
+      "ClaudeCodeVerbose"
+    ];
 
-        settings = {
-          window = {
-            position = "vertical";
-          };
-        };
+    settings = {
+      window = {
+        position = "vertical";
       };
-    }
-    (lib.mkIf config.plugins.claude-code.enable {
-      keymaps = import ./keymaps.nix;
-    })
-  ];
+    };
+  };
+  # keymaps = import ./keymaps.nix;
 }
