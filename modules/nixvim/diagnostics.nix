00b70c80da66c28c16768e7615723b520cd964b2
woxQAQ <woxqaq@gmail.com>
2026-02-18 21:43:46 +0800

    chore: disable inline diagnostics

--- modules/nixvim/diagnostics.nix
+++ modules/nixvim/diagnostics.nix
@@ -2,13 +2,13 @@
   diagnostic.settings = {
     update_in_insert = true;
     severity_sort = true;
-    virtual_text = {
-      severity.min = "warn";
-      source = "if_many";
-    };
-    virtual_lines = {
-      current_line = true;
-    };
+    # virtual_text = {
+    #   severity.min = "warn";
+    #   source = "if_many";
+    # };
+    # virtual_lines = {
+    #   current_line = true;
+    # };
     float.border = "rounded";
     jump = {
       severity.__raw = "vim.diagnostic.severity.WARN";
