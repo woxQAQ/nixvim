d344e5504dc6ab3f3183905a9883c108c54bc466
woxQAQ <woxqaq@gmail.com>
2026-02-19 22:15:04 +0800

    chore: avante unset provider

--- modules/nixvim/plugins/avante/default.nix
+++ modules/nixvim/plugins/avante/default.nix
@@ -11,16 +11,6 @@
       lazyLoad.settings.event = [ "DeferredUIEnter" ];
 
       settings = {
-        provider = "claude-code";
-        acp_providers = {
-          claude-code = {
-            model = "claude-sonnet-4-5";
-            env = {
-              ANTHROPIC_API_KEY.__raw = ''os.getenv("ANTHROPIC_API_KEY")'';
-              ANTHROPIC_BASE_URL.__raw = ''os.getenv("ANTHROPIC_BASE_URL")'';
-            };
-          };
-        };
         # Define our own mappings under correct prefix
         mappings = {
           ask = "<leader>aaa";
